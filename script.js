55a60d9be47a0c132cba32e7c8f9ac335d8c8dfa
Yaser Aly <yaser-aly@live.com>
2023-11-07 22:22:03 -0800

    created all variables in script

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
